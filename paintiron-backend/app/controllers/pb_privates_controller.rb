24f35eb0e838b258f13ea94b46be1e87e508203a
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-02-16 08:26:02 -0500

    added show create update and destroy on multiple controllers

--- paintiron-backend/app/controllers/pb_privates_controller.rb
+++ paintiron-backend/app/controllers/pb_privates_controller.rb
@@ -1,7 +1,30 @@
 class PbPrivatesController < ApplicationController
-    def index
-        render json: PbPrivate.all.to_json(pb_private_serializer_options)
-    end
+   def index
+      render json: PbPrivate.all.to_json(pb_private_serializer_options)
+   end
+
+   def show
+      pbPrivate = PbPrivate.find(params[:id]) 
+      render json: pbPrivate.to_json(pb_private_serializer_options) 
+   end
+
+   def create 
+      pbPrivate = PbPrivate.new(pb_private_serializer_options)
+      pbPrivate.save
+      render json: pbPrivate.to_json(pb_private_serializer_options)
+      end
+   end
+
+   def update
+      pbPrivate = PbPrivate.find(params[:id])
+      pbPrivate.update(pb_private_serializer_options)
+      render json: pbPrivate.to_json(pb_private_serializer_options)
+   end
+
+   def destroy
+      pbPrivate = PbPrivate.find(params[:id])
+      pbPrivate.destroy
+  end
 
     private
 
--- paintiron-backend/app/controllers/user_palettes_controller.rb
+++ paintiron-backend/app/controllers/user_palettes_controller.rb
@@ -1,9 +1,32 @@
 class UserPalettesController < ApplicationController
-    def index
-        render json: UserPalette.all.to_json(user_palette_serializer_options)
-    end
+   def index
+      render json: UserPalette.all.to_json(user_palette_serializer_options)
+   end
 
-    private
+   def show
+      userPalette = UserPalette.find(params[:id])
+      render json: userPalette.to_json(user_palette_serializer_options) 
+   end 
+
+   def create 
+      userPalette = UserPalette.new(user_palette_serializer_options)
+      userPalette.save
+      render json: userPalette.to_json(user_palette_serializer_options)
+      end
+   end
+
+   def update
+      userPalette = UserPalette.find(params[:id])
+      userPalette.update(user_palette_serializer_options)
+      render json: userPalette.to_json(user_palette_serializer_options)
+   end
+
+   def destroy
+      userPalette = UserPalette.find(params[:id])
+      userPalette.destroy
+   end
+
+   private
 
    def user_palette_serializer_options()
       {
